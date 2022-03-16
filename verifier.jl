5020dcef4945a55fa68dc7aa892ed7612e7fc88d
Ben Cardoen <26388277+bencardoen@users.noreply.github.com>
2022-03-16 13:15:23 +0000

    Formatter step

--- verifier.jl
+++ verifier.jl
@@ -27,39 +27,36 @@ for i in [1]
     end
 end
 
+
 function formatpath(x)
     parts = splitpath(x)
-
+    parts[end] = "$(read_postfix_int(parts[end]))"
+    return joinpath(parts...)
 end
 
 
-
-FR = r"[-+]?([0-9]*[.])?[0-9]+([eE][-+]?\d+)?"
-match(FR, "234.34").match
 
 ## Make counters to keep track of what changed
 ec, count_error = generate_counter(true)
 cc, count_correct = generate_counter(true)
 sc, count_size = generate_counter(true; incrementer=size_of_file)
 ## SLURM needs lists of paths to process, let's build those
-inlist = make_shared_list()
-outlist = make_shared_list()
+inlist, outlist = [make_shared_list() for _ in 1:2]
 ## Define where the output will be written
 outpath = mktempdir()
 
 ## Define conditions and actions we want to take
 
-### Incorrect entries -> log
-record_fail = x->log_to_file("errors.txt", x)
-### Same for correct
-record_correct = x->log_to_file("correct.txt", x)
-### Warn + count + write filename, then delete
+### Create a log for both failed entries and valid entries
+record_fail, record_correct = [x->log_to_file(X, x) for X in ["errors.txt", "correct.txt"]]
+
+### Invalid data : warn, count, log and delete
 onfail = x -> apply_all([warn_on_fail, count_error, record_fail, delete_file], x)
 
-### Collect input and output paths for correct entries
-on_input_dir = x -> apply_all([x->addentry!(inlist, x), x->addentry!(outlist, new_path(root, x, outpath))], x)
+### Create 2 lists of input directories and output directories for the pipeline
+on_input_dir = x -> apply_all([x->addentry!(inlist, x), x->addentry!(outlist, new_path(root, formatpath(x), outpath))], x)
 
-### On valid entries: Count, count size, log, and move on
+### On valid entries: Count nr and size, log
 onsuccess = x -> apply_all([count_correct, count_size, record_correct], x)
 
 
