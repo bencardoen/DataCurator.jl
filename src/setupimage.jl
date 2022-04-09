d71a53fee66980d97b8f5de41d4f68e2c8cf1623
Ben Cardoen <26388277+bencardoen@users.noreply.github.com>
2022-04-09 15:15:32 +0100

    Add tests as a better precompilation target + remove packages with v2 of PC

--- src/setupimage.jl
+++ src/setupimage.jl
@@ -5,4 +5,6 @@ Pkg.activate(".")
 # create_sysimage([:Images, :CSV, :DataFrames, :DataCurator, :ArgParse, :TOML, :HDF5, :MAT], sysimage_path="sys_img.so", precompile_execution_file="./src/precompile.jl")
 #
 @info pwd()
-create_sysimage([:Images, :CSV, :DataFrames, :DataCurator, :ArgParse, :TOML, :HDF5, :MAT], sysimage_path="sys_img.so", include_transitive_dependencies=false, precompile_statements_file="dc_precompile.jl")
+create_sysimage(sysimage_path="sys_img.so", include_transitive_dependencies=false, precompile_statements_file="dc_precompile.jl")
+#
+# create_sysimage([:Images, :CSV, :DataFrames, :DataCurator, :ArgParse, :TOML, :HDF5, :MAT], sysimage_path="sys_img.so", include_transitive_dependencies=false, precompile_statements_file="dc_precompile.jl")
