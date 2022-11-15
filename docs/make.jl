ebc6abe84ba67bbd767ce8a52bb1f59cecd25371
Ben Cardoen <26388277+bencardoen@users.noreply.github.com>
2022-11-15 13:12:13 -0800

    add missing docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter
 using DataCurator
 Documenter.makedocs(root="./", source="src", build="build", clean=true,
 modules=Module[DataCurator], sitename="DataCurator Documentation",
-format = Documenter.HTML(prettyurls = false), pages=["Index"=> "index.md", "Installation"=>"installation.md", "Quikstart" => "usage.md","Documented recipe with all features demonstrated"=>"recipe.md", "Conditions and Actions for use in recipes"=>"conditions.md","Remote configuration" => "remote.md", "API Reference"=> "reference.md"])
+format = Documenter.HTML(prettyurls = false), pages=["Index"=> "index.md", "Installation"=>"installation.md", "Quikstart" => "usage.md","Documented recipe with all features demonstrated"=>"recipe.md", "Conditions and Actions for use in recipes"=>"conditions.md","Remote configuration" => "remote.md", ""=>"extending.md", "API Reference"=> "reference.md"])
 #
 # deploydocs(
 #     repo = "github.com/bencardoen/DataCurator.jl.git",
